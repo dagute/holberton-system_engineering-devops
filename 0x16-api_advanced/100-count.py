796f1d15bbd3bbb5f329ffaf9b92cfb85990beee
dagute <1271@holbertonschool.com>
2020-05-07 23:57:39 +0000

    Count it

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''Count it'''
+import requests
+import sys
+
+
+def search(subreddit, word):
+    """ parses the title of all hot articles, and prints
+    a sorted count of given keywords
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, word.get('after'))
+    user_agent = {'User-Agent': 'dagute'}
+
+    req = requests.get(url, headers=user_agent).json()
+
+    my_list = req.get('data').get('children')
+    for arg in my_list:
+        title = arg.get('data').get('title').lower().split(' ')
+        for key, value in word.items():
+            if key != 'after':
+                word[key] += title.count(key)
+    after = req.get('data').get('after')
+    if after:
+        word['after'] = after
+        search(subreddit, word)
+    else:
+        for key, value in sorted(word.items()):
+            if value is not 0 and key is not 'after':
+                print("{}: {}".format(key, value))
+
+
+def count_words(subreddit, word_list):
+    """counts words in a subreddit"""
+    word = {}
+    for arg in word_list:
+        word[arg] = 0
+    word['after'] = ''
+    search(subreddit, word)
