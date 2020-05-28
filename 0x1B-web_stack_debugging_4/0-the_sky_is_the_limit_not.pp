fc6b06816817e7c4c88bafa31ec2f2897a5acd8d
dagute <1271@holbertonschool.com>
2020-05-28 18:47:16 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,11 +3,11 @@
 exec { 'increase':
   command  => "sed -i 's/15/5000/g' /etc/default/nginx",
   path     => '/bin',
-  provider => shell
+  provider => 'shell'
 }
 
 exec { 'restart':
   command  => 'nginx restart',
-  path     => '/etc/init.d/'
-  provider => shell
+  path     => '/etc/init.d/',
+  provider => 'shell'
 }
