07e742d76fe8f912e1f70c8040cd3ba2cbae2062
dagute <1271@holbertonschool.com>
2020-05-28 22:13:52 +0000

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-#Grow the limit of requests
+#Change the user limitations
 
 exec { 'permissions':
   command => "sed -i 's/4/32000/g' /etc/security/limits.conf && sed -i 's/5/32000/g' /etc/security/limits.conf",
